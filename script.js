8f7e1924e0b884779d500ae1dfaecae3dbdb9067
Ziv Tamary <zivtamary@gmail.com>
2021-07-08 13:58:04 +0300

    delete unused vars

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 const canvas = document.getElementById("canvas");
-const increaseBtn = document.getElementById("increase");
-const decreaseBtn = document.getElementById("decrease");
-const sizeEL = document.getElementById("size");
 const colorEl = document.getElementById("color");
 const clearEl = document.getElementById("clear");
 
